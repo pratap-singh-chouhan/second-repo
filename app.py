0d4b3ac89f538e6e11a12c5909999cf1efc8bb66
Pratap Singh Chouhan <pratapbanna963@gmail.com>
2025-10-20 15:44:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app= Flask(__name__)
 
 @app.route("/")
 def index():
-    return helllo this is my first flask web server
+    return helllo it is my first flask web server
 
 if __name__=="__main__":
     app.run(debug=True)
